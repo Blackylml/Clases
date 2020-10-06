94f7a04fcd706316ac8305b496f4acc08623ebc5
Blackylml <70767701+Blackylml@users.noreply.github.com>
2020-10-06 12:36:26 -0500

    Agregar archivos de proyecto.

---
+++ Clases/Metodos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Metodos
+    {
+        //atributos
+
+        //metodo consturctor
+        public Metodos()
+        {
+            Console.WriteLine("creando objeto");
+        }
+
+
+
+
+
+        //metodos
+        public void mensaje()
+        {// iniciador de bloque de codigo
+
+            Console.WriteLine("buenas tardes");
+
+        }//finalizacion de bloque de codigo
+
+
+    }
+}
---
+++ Clases/Program.cs
@@ -0,0 +1,30 @@
+﻿ //bibliotecas
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ //proyecto
+namespace Clases
+{
+    //clase
+    class Program
+    {
+        //metodo
+        //static = global
+        //void = sin registro de valores
+        //main = nombre del metodo
+        //(argumentos que hara uso el meyodo)
+        static void Main(string[] args)
+        {
+            Metodos m = new Metodos(); //Construyendo objeto
+
+            m.mensaje();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
